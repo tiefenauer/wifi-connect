9012caa574b80759645e854ce92ed51d0740a647
tiefenauer <tiefenauer@users.noreply.github.com>
2023-01-28 12:33:59 +0000

    Apply automatic changes

--- ui/build/service-worker.js
+++ ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96ed21ec05e3b25e96ee640119b0334b.js"
+  "/precache-manifest.e45c5f10a29112444e57168ef0d2b3cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
